80e70aecc1be9b35e49234ae17f3284ccaf641a7
BT_1N3_07 <bsmith3816@bths.edu>
2018-09-25 10:18:07 -0400

    9/25/18 #2

--- src/com/company/Part2.java
+++ src/com/company/Part2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Part2 {
     public static void main(String[] args) {
+        boolean finished = false;
         int numGuess = 1;
         int num = 50;
         int min = 1;
@@ -11,19 +12,26 @@ public class Part2 {
         int x;
         Scanner input = new Scanner(System.in);
         String reply;
+
         System.out.println("Welcome to the Guessing Game! I will try and guess a number you choose from 1 to 100. Please type in 'higher','lower' or 'correct' as I make guesses");
-        boolean finished = false;
+
         while(!finished){
             System.out.println("Is the number " + num + "?");
             reply = input.nextLine();
             if(reply.equals("correct")){
                 finished = true;
             }
+
             else if(reply.equals("higher")){
                 numGuess++;
                 min = num;
-                x = max - min;
-                if(x == 1){
+                x = max - min - 1;
+                if(x < 0){
+                    System.out.println("Impossible! This has to be the number!");
+                    finished = true;
+                }
+                 */
+                /* if(x == 1){
                     num++;
                     while(!finished) {
                         System.out.println("The number has to be " + num + ".");
@@ -34,10 +42,11 @@ public class Part2 {
                             System.out.println("There's no way it can be any other number!");
                         }
                     }
-                }
+                } */
                 num = (int) Math.floor((Math.random() * x) + 1);
                 num += min;
             }
+
             else if(reply.equals("lower")) {
                 numGuess++;
                 max = num;
@@ -45,13 +54,17 @@ public class Part2 {
                 num = (int) Math.floor((Math.random() * x) + 1);
                 num += min;
             }
+
             else {
                 System.out.println("Please type either correct, higher or lower, or else I won't be able to answer!");
             }
         }
+
         if(numGuess == 1){
             System.out.println("Hooray! I have guessed the number in just one shot!");
-        } else {
+        }
+
+        else {
             System.out.println("Hooray! I have guessed the number in " + numGuess + " tries!");
         }
     }
