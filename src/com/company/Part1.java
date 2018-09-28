b4000dc1f4e535997327b093f83e43f29d24ab78
BT_1N3_07 <bsmith3816@bths.edu>
2018-09-28 09:50:36 -0400

    9/28 #2

--- src/com/company/Part1.java
+++ src/com/company/Part1.java
@@ -8,21 +8,26 @@ public class Part1 {
         Scanner input = new Scanner(System.in);
         Double random = Math.floor(Math.random() * 25) + 1;
         String thenumber = Double.toString(random);
-        thenumber = thenumber.substring(0,1);
+        thenumber = thenumber.substring(0,2);
+        if(thenumber.contains(".")){
+            thenumber = thenumber.substring(0,1);
+        }
         boolean answer = false;
-        String guess;
         System.out.println("Welcome to the Guessing Game! What's your name?");
         String name = input.nextLine();
         System.out.println("Hello, " + name + "!");
         System.out.println("Try and guess the integer I'm thinking of, from 1 to 25.");
+        String guess;
+        int numGuess = 1;
         while(!answer){
             guess = input.nextLine();
             if(guess.equals(thenumber)){
                 answer = true;
             } else {
-                System.out.println("Oh, that was the wrong number! Try again, " + name + "!");
+                numGuess++;
+                System.out.println("Oh, that was the wrong value! Try again, " + name + "!");
             }
         }
-        System.out.println("Good job, " + name + "! You guessed the correct number! You deserve a cookie : )");
+        System.out.println("Good job, " + name + "! You guessed the correct number in " + numGuess + " tries! You deserve a cookie : )");
     }
 }
