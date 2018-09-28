5f1489b4d96c98950d7a219af6f0e38a39220c75
BT_1N3_07 <bsmith3816@bths.edu>
2018-09-28 09:42:42 -0400

    9/28

--- src/com/company/Part1.java
+++ src/com/company/Part1.java
@@ -6,20 +6,23 @@ public class Part1 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Double random = Math.floor(Math.random() * 10) + 1;
+        Double random = Math.floor(Math.random() * 25) + 1;
         String thenumber = Double.toString(random);
         thenumber = thenumber.substring(0,1);
         boolean answer = false;
         String guess;
-        System.out.println("Try and guess the integer I'm thinking of, from 1 to 10.");
+        System.out.println("Welcome to the Guessing Game! What's your name?");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Try and guess the integer I'm thinking of, from 1 to 25.");
         while(!answer){
             guess = input.nextLine();
             if(guess.equals(thenumber)){
                 answer = true;
             } else {
-                System.out.println("Oh, that was the wrong number! Try again.");
+                System.out.println("Oh, that was the wrong number! Try again, " + name + "!");
             }
         }
-        System.out.println("Good job! You guessed the correct number! You deserve a cookie : )");
+        System.out.println("Good job, " + name + "! You guessed the correct number! You deserve a cookie : )");
     }
 }
--- src/com/company/Part2A.java
+++ src/com/company/Part2A.java
@@ -5,17 +5,11 @@ import java.util.Scanner;
 public class Part2A {
     public static int guessRandom(int low,int high){
         int x = high - low;
-        if(x == 1){
-            return low;
-        }
-        if(x == 0){
-            return high;
-        }
         int num = (int) Math.floor((Math.random() * x) + 1);
-        if(num == low) {
+        if(num + low == low) {
             num++;
         }
-        if(num == high){
+        if(num + low == high){
             num--;
         }
         return num + low;
@@ -24,35 +18,41 @@ public class Part2A {
         boolean finished = false;
         int numGuess = 1;
         int num = 50;
-        int min = 1;
+        int min = 0;
         int max = 100;
-        int x;
         Scanner input = new Scanner(System.in);
         String reply;
 
-        System.out.println("Welcome to the Guessing Game! I will try and guess a number you choose from 1 to 100. Please type in 'higher','lower' or 'correct' as I make guesses");
+        System.out.println("Welcome to the Guessing Game! What's your name?");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
+        System.out.println("I will try and guess a number you choose from 1 to 100. Please type in 'higher','lower' or 'correct' if the number you have in mind is higher, lower, or is correct respectively.");
 
         while(!finished){
-            System.out.println("Is the number " + num + "?");
+            System.out.println(name + ", is the number " + num + "?");
             reply = input.nextLine();
             if(reply.equals("correct")){
                 finished = true;
             }
 
             else if(reply.equals("higher")){
-                if(num - max == -1 || num == 100){
-                    System.out.println("The number can't be any higher than this! Ending game due to user error.");
+                if(num == 99){
+                    min = num;
+                    numGuess++;
+                    num++;
+                } else if(num - max == -1 || num == 100){
+                    System.out.println("The number can't be any higher than this! Ending game due to user error. Sorry, " + name + "!");
                     System.exit(0);
+                } else {
+                    numGuess++;
+                    min = num;
+                    num = guessRandom(min, max);
                 }
-                numGuess++;
-                min = num;
-                num = guessRandom(min,max);
-
             }
 
             else if(reply.equals("lower")) {
                 if(num - min == 1 || num == 0){
-                    System.out.println("The number can't be any lower than this! Ending game due to user error.");
+                    System.out.println("The number can't be any lower than this! Ending game due to user error. Sorry, " + name + "!");
                     System.exit(0);
                 }
                 numGuess++;
@@ -62,16 +62,16 @@ public class Part2A {
             }
 
             else {
-                System.out.println("Please type either correct, higher or lower, or else I won't be able to answer!");
+                System.out.println("Hey " + name + ", please type either correct, higher or lower, or else I won't be able to answer!");
             }
         }
 
         if(numGuess == 1){
-            System.out.println("Hooray! I have guessed the number in just one shot!");
+            System.out.println("Hooray! I have guessed the number in just one shot! GG, " + name + "!");
         }
 
         else {
-            System.out.println("Hooray! I have guessed the number in " + numGuess + " tries!");
+            System.out.println("Hooray! I have guessed the number in " + numGuess + " tries! Thanks for playing, " + name + "!");
         }
     }
 }
